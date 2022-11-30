52d7ec8ad7ed0f3e3bb5fc9f55630f61598502d7
HelloGit <HelloGit@qq.com>
2022-11-30 13:13:32 +0800

    first commit

---
+++ src/main/java/com/cjt/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.cjt.git;
+
+/**
+ * @author Mr cai
+ * @Decription
+ * @create 2022-11-30-13:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+
+    }
+}
